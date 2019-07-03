c963c37ed23dc107fc6efcbe9763a3c4714baee9
prasanthsunny <51721086+prasanthsunny341@users.noreply.github.com>
2019-07-03 22:19:21 +0530

    Created the Bank_Marketing_Strategy
     Code Inprogress

---
+++ Python/case_study/Bank_Marketing_Strategy.py
@@ -0,0 +1,5 @@
+userentry =input("Please enter your username")
+print("YOUR string/message IS :", userentry)
+#reverse the string
+REVERSE=userentry[::-1]
+print("Reverse string for your Input",REVERSE)
